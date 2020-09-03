b914a685e141296d74fafb88875fb462bab22f35
inderjeet <b18058@students.iitmandi.ac.in>
2020-09-03 23:37:55 +0530

    commit-9

---
+++ CF-666-Div2/B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
+    return os << "(" << P.first << "," << P.second << ")"; }
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {
+    os << "[ "; for(auto v : V) os << v << " "; return os << "]"; }
+template<class T> ostream& operator<<(ostream &os, set<T> S){
+    os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}"; }
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {
+    os << "{ "; for(auto m:M) os<<"("<<m.F<<":"<<m.S<<") "; return os<<"}"; }
+#define db(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " |"; __f(comma + 1, args...); }
+#define rng(x) x.begin(), x.end()
+#define pb push_back
+#define F first
+#define S second
+#define el '\n'
+ 
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+ 
+/*-----------------------------Code begins----------------------------------*/
+
+void solve1(){
+	int n; cin>>n;
+	vi a(n); for(int &i : a) cin>>i;
+	sort(rng(a));
+	ll ci1 = pow(a.back(), 1.0/(n-1)), ci2 = ci1 + 1;
+	ll s1 = 0, s2 = 0, f1 = 1, f2 = 1;
+	for(int i : a) s1 += abs(i - f1), f1 *= ci1;
+	for(int i : a) s2 += abs(i - f2), f2 *= ci2;
+	cout<<min(s1, s2)<<el;
+}
+
+void solve(){
+	int n; cin>>n;
+	vl a(n); for(ll &i : a) cin>>i;
+	sort(rng(a));
+	ll lsm = LLONG_MAX, c = 1;
+	while(true){
+		ll ci = 1, sm = 0;
+		for(int i : a){
+			sm += abs(i - ci), ci *= c;
+			if(sm >= lsm) break;
+		} 
+		if(sm >= lsm) break;
+		lsm = sm;
+		c++;
+	}
+	cout<<lsm<<el;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int T=1, tc = 1;
+    // cin>>T; 
+    while(T--){
+        solve();
+    }
+    return 0; 
+}
---
+++ CF-666-Div2/C.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
+    return os << "(" << P.first << "," << P.second << ")"; }
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {
+    os << "[ "; for(auto v : V) os << v << " "; return os << "]"; }
+template<class T> ostream& operator<<(ostream &os, set<T> S){
+    os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}"; }
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {
+    os << "{ "; for(auto m:M) os<<"("<<m.F<<":"<<m.S<<") "; return os<<"}"; }
+#define db(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " |"; __f(comma + 1, args...); }
+#define rng(x) x.begin(), x.end()
+#define pb push_back
+#define F first
+#define S second
+#define el '\n'
+ 
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+ 
+/*-----------------------------Code begins----------------------------------*/
+
+// tricky question;
+
+void solve(){
+    int n; cin>>n;
+    vl a(n); for(ll &i : a) cin>>i;
+    if(n==1){
+    	cout<<"1 1\n0\n1 1\n0\n1 1\n"<<-a[0]<<el;
+    	return;
+    }
+    cout<<"1 1\n"<<-a[0]<<el;
+    cout<<"1 "<<n<<el<<"0 ";
+    for (int i = 1; i < n; ++i){
+    	cout<<-n*a[i]<<" ";
+    }
+    cout<<el<<"2 "<<n<<el;
+    for (int i = 1; i < n; ++i){
+    	cout<<(n-1)*a[i]<<" ";
+    }
+    cout<<el;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int T=1, tc = 1;
+    // cin>>T; 
+    while(T--){
+        solve();
+    }
+    return 0; 
+}
